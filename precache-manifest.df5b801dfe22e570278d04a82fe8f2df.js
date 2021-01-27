70f09d0d8545c8ba89035ffd2e6e2d47a8c697cd
saurav <avisaurav485@gmail.com>
2021-01-27 01:58:53 -0600

    Updates

---
+++ precache-manifest.df5b801dfe22e570278d04a82fe8f2df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb3c07754374f84a56d1070ab2dd90b",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "280229dbbd59bd873513",
+    "url": "/weather-app/static/css/main.e7104647.chunk.css"
+  },
+  {
+    "revision": "defb93430075387fb1f9",
+    "url": "/weather-app/static/js/2.6a804ba5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.6a804ba5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "280229dbbd59bd873513",
+    "url": "/weather-app/static/js/main.10d38fb5.chunk.js"
+  },
+  {
+    "revision": "bdcb51b63d9249268df7",
+    "url": "/weather-app/static/js/runtime-main.f1c906e7.js"
+  }
+]);
